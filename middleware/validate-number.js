f7ba8227bf897227154db45c3615360e1666ba2f
Mohammad-Almomani <moh.moma.mmm@gmail.com>
2022-09-07 09:14:23 +0300

    fixed a bug

--- middleware/validate-number.js
+++ middleware/validate-number.js
@@ -2,8 +2,9 @@
 
 module.exports = () => {
   return (req, res, next) => {
-    if (!isNaN(req.query.num)) {
-      req.squareNum = Math.pow(req.query.num, 2);
+    if (!isNaN(+req.query.num)) {
+      console.log(`${+req.query.num} is a ${typeof +req.query.num}`);
+      req.squareNum = Math.pow(+req.query.num, 2);
       next();
     } else {
       next(`${req.query.num} is not a number`);
